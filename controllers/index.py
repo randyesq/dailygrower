db66fceaed1a8bbe46a56daef2a39cf98fa4b80b
Randy Kleinman <randy.kleinman@gmail.com>
2020-11-17 22:36:52 -0600

    Subscribe link direct to buttondown; minor fixes

--- controllers/index.py
+++ controllers/index.py
@@ -10,6 +10,7 @@ from dailygrower.airtable import fetch_links, get_link_tags
 from dailygrower.links import weight_links, parse_netloc
 
 template_name = os.environ.get('TEMPLATE_NAME', 'index.html.j2')
+fetch_links_from_db = os.environ.get('FETCH_LINKS', False)
 output_dir = Path(os.environ.get('OUTPUT_DIR', Path(__file__).parent.parent.joinpath('output')))
 
 env = Environment(
@@ -20,11 +21,14 @@ env = Environment(
 
 LINKS_PER_PAGE = 5
 
-# Fetch the links from the database
-links = fetch_links(view="Live", table="Content")
-ads = fetch_links(view="Live", table="Advertisements")
-links = weight_links(links)
-links = parse_netloc(links)
+# Fetch the links and advertisements from the database
+links = []
+ads = []
+if fetch_links_from_db:
+    links = fetch_links(view="Live", table="Content")
+    ads = fetch_links(view="Live", table="Advertisements")
+    links = weight_links(links)
+    links = parse_netloc(links)
 
 
 # Get the date in the CST/CDT timezone
@@ -38,6 +42,7 @@ template_globals = {
     'tags': get_link_tags(links),
     'links_per_page': LINKS_PER_PAGE,
     'link_pagination_pages': math.ceil(len(links) / LINKS_PER_PAGE),
+    'subscribe_url': 'https://buttondown.email/dailygrower',
     'ENABLE_GOOGLE_ADS': False,
     'ENABLE_GOOGLE_LINK_TRACKING': True,
     'ENABLE_TAGS': False,
--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ export PYTHONPATH=`pwd`
 rm -rf ${OUTPUT_DIR} && mkdir -p ${OUTPUT_DIR}/static
 pip3 install -r requirements.txt
 cp -r static/* ${OUTPUT_DIR}/static
-TEMPLATE_NAME=index.html.j2 python3 controllers/index.py
-TEMPLATE_NAME=submit.html.j2 python3 controllers/index.py
-TEMPLATE_NAME=subscribe.html.j2 python3 controllers/index.py
+TEMPLATE_NAME=index.html.j2 FETCH_LINKS=true python3 controllers/index.py
+TEMPLATE_NAME=contribute.html.j2 python3 controllers/index.py
+TEMPLATE_NAME=subscribe-thanks.html.j2 python3 controllers/index.py
 TEMPLATE_NAME=contrib-thanks.html.j2 python3 controllers/index.py
