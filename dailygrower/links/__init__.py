d8a5e45aa7a0365ab878b0514a02549818d0fe30
Randy Kleinman <randy.kleinman@gmail.com>
2021-01-27 23:23:32 -0600

    Deserialize link data; make links an object

---
+++ dailygrower/links/__init__.py
@@ -0,0 +1,82 @@
+# Objects used in the Daily Grower Site
+from datetime import datetime
+from urllib.parse import urlparse, parse_qsl
+
+from marshmallow import Schema, fields, post_load, pre_load
+
+
+class Link(object):
+    """ Object representation of a Link entry """
+    def __init__(self, *args, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+        # Additional massaging
+        self.tags = self.tags or []
+
+        # Additional fields besides the schema-defined fields
+        self.netloc = urlparse(self.url).netloc
+
+        # Additional processing of schema-defined fields
+        # If this is a YouTube link and there is no other image
+        # set for it, get the YouTube thumbnail
+        if self.is_youtube_link() and self.image_url is None:
+            self.image_url = self.fetch_youtube_thumbnail()
+
+    def __repr__(self):
+        return "<Link(headline={self.headline!r})>".format(self=self)
+
+    def is_youtube_link(self):
+        return any([nl in self.netloc for nl in ['youtube.com', 'youtu.be'] ])
+
+    def fetch_youtube_thumbnail(self):
+        """
+        Fetch a youtube thumbmail image
+        # https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
+        """
+        vid = dict(parse_qsl(urlparse(self.url).query))['v']
+        return 'https://i.ytimg.com/vi_webp/{}/maxresdefault.webp'.format(vid)
+
+
+class LinkSchema(Schema):
+    """ Schema for a Link """
+    id = fields.Str(required=True)
+    url = fields.Url(required=True)
+    tags = fields.List(fields.Str(), missing=None)
+    headline = fields.Str(required=True)
+    blurb = fields.Str(required=True)
+    featured = fields.Boolean(default=False, missing=None)
+    approved = fields.Boolean(default=False, missing=None)
+    approval_date = fields.DateTime(missing=None)
+    archived = fields.Boolean(default=False, missing=None)
+    archive_date = fields.DateTime(missing=None)
+    image_url = fields.Url(missing=None)
+    image_alt = fields.Str(missing=None)
+    created_date = fields.DateTime(required=True)
+
+    class Meta:
+        datetimeformat = '%Y-%m-%dT%H:%M:%S.000Z'
+
+    @pre_load
+    def unwrap_airtable_data_structure(self, raw_data, **kwargs):
+        """ Translate an Airtable entry to a deserialized representation """
+        data =  {
+            'id': raw_data['id'],
+            'url': raw_data['fields']['Link'],
+            'headline': raw_data['fields']['Headline'],
+            'blurb': raw_data['fields']['Blurb'],
+            'created_date': raw_data['createdTime']
+        }
+        data['featured'] = raw_data['fields'].get('Featured', None)
+        data['tags'] = raw_data['fields'].get('Link Tags', None)
+        data['approved'] = raw_data['fields'].get('Approved', None)
+        data['approval_date'] =raw_data['fields'].get('Approval Date', None)
+        data['archived'] = raw_data['fields'].get('Archived', None)
+        data['archive_date'] =raw_data['fields'].get('Archive Date', None)
+        data['image_url'] = raw_data['fields'].get('Image URL', None)
+        data['image_alt'] = raw_data['fields'].get('Image Alt', None)
+        return data
+
+    @post_load
+    def make_link(self, data, **kwargs):
+        return Link(**data)
--- dailygrower/render/filters.py
+++ dailygrower/render/filters.py
@@ -4,18 +4,14 @@ from urllib.parse import urlparse
 import pytz
 
 
-def netloc(url):
-    """ Parse the net location of a url """
-    return urlparse(url).netloc
-
-
 def approval_day(link, tz):
     """
     Get the day of the week (Monday, Tuesday) etc when the link was approved
     """
     LOCAL_TZ = pytz.timezone(tz)
-    local_date = link['fields']['Approval Date'].replace(tzinfo=pytz.utc).astimezone(LOCAL_TZ)
+    local_date = link.approval_date.replace(tzinfo=pytz.utc).astimezone(LOCAL_TZ)
     return local_date.strftime('%A')
+
 
 def make_ordinal(n):
     '''
