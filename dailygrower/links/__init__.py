902be8fb9efe034e7cbb8f894ea9f65560cbb488
Randy Kleinman <randy.kleinman@gmail.com>
2021-01-30 23:07:01 -0600

    Get channel name for youtube links from youtube data api

--- dailygrower/links/__init__.py
+++ dailygrower/links/__init__.py
@@ -1,8 +1,13 @@
 # Objects used in the Daily Grower Site
 from datetime import datetime
+import os
 from urllib.parse import urlparse, parse_qsl
 
 from marshmallow import Schema, fields, post_load, pre_load
+import requests
+
+
+YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
 
 
 class Link(object):
@@ -20,8 +25,14 @@ class Link(object):
         # Additional processing of schema-defined fields
         # If this is a YouTube link and there is no other image
         # set for it, get the YouTube thumbnail
-        if self.is_youtube_link() and self.image_url is None:
-            self.image_url = self.fetch_youtube_thumbnail()
+        if self.is_youtube_link():
+            if self.image_url is None:
+                self.image_url = self.fetch_youtube_thumbnail()
+
+            # For YouTube Links, set the netloc to the channel name instead of
+            # the YouTube domain name
+            channel = self.fetch_youtube_channel_name()
+            self.netloc = "YouTube: {}".format(channel)
 
     def __repr__(self):
         return "<Link(headline={self.headline!r})>".format(self=self)
@@ -36,6 +47,24 @@ class Link(object):
         """
         vid = dict(parse_qsl(urlparse(self.url).query))['v']
         return 'https://i.ytimg.com/vi_webp/{}/maxresdefault.webp'.format(vid)
+
+    def fetch_youtube_channel_name(self):
+        """
+        Use the YouTube Data API to get the name of the channel based on the
+        video id
+        """
+        vid = dict(parse_qsl(urlparse(self.url).query))['v']
+        resp = requests.get(
+            'https://youtube.googleapis.com/youtube/v3/videos',
+            headers={"Content-type": "application/json"},
+            params={
+                "key": YOUTUBE_API_KEY,
+                "part": "snippet",
+                "id": vid
+            }
+        )
+        resp.raise_for_status()
+        return resp.json()['items'][0]['snippet']['channelTitle']
 
 
 class LinkSchema(Schema):
