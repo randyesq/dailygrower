f20fee07ba8412010967adb0658329ac8421f896
Randy Kleinman <randy.kleinman@gmail.com>
2021-02-16 23:32:00 -0600

    Publish to DG Facebook page

--- dailygrower/builds/__init__.py
+++ dailygrower/builds/__init__.py
@@ -40,6 +40,9 @@ def build_weekday(config):
         # Create daily digest subscriber email
         send_daily_link_email(config, next_link)
 
+        # Post to Facebook
+        create_facebook_post(config, next_link)
+
 
 def build_rollup(config):
     """ Rollup build, send weekly digest email, typically a Saturday """
@@ -77,3 +80,26 @@ def _get_link_views(config, pending=False, current=False, archived=False):
         )
 
     return link_content
+
+def create_facebook_post(config, next_link):
+    import facebook
+    import datetime
+    fb_page = config['facebook_page_id']
+    access_token = os.environ.get("FACEBOOK_ACCESS_TOKEN")
+    graph = facebook.GraphAPI(access_token=access_token, version="2.12")
+    then = datetime.datetime.now() + datetime.timedelta(hours=1)
+    link = next_link[0]
+    message = """
+    Today on The Daily Grower:
+
+    {}
+
+    Check it out at https://dailygrower.com
+    """.format(link.headline)
+    print(graph.put_object(
+        fb_page,
+        "feed",
+        published=False,
+        message="Hello, world",
+        scheduled_publish_time=then.replace(microsecond=0).isoformat()
+    ))
